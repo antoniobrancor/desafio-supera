84231338659ec72736f07be58b45a3a29976e5de
Antonio Branco <antonio.branco.0@gmail.com>
2023-07-19 12:56:03 -0300

    Implementa resolução do segundo desafio

--- src/segundoDesafio/SegundoDesafio.java
+++ src/segundoDesafio/SegundoDesafio.java
@@ -1,4 +1,90 @@
 package segundoDesafio;
 
+import java.util.Scanner;
+
 public class SegundoDesafio {
+    public static void main(String[] args) {
+        executarDesafio();
+
+    }
+
+    //Método para chamar a resolução do desafio lendo input do valor
+    private static void executarDesafio() {
+        Scanner scanner = new Scanner(System.in);
+        float valor = scanner.nextFloat();
+        int centavos = Math.round(valor * 100);
+        contarNotas((centavos));
+    }
+
+    private static void contarNotas(int centavos) {
+
+        //Inicialização das quantidades de notas
+        int nota100, nota50, nota20, nota10, nota5,nota2, moeda1, moeda50, moeda25, moeda10, moeda5, moeda01;
+
+
+        System.out.println("NOTAS:");
+        //Vai de valor em valor vendo se as notas serão utilizadas, a impressão acontece de qualquer forma
+        if (centavos % 10000 >= 0) {
+            nota100 = centavos / 10000;
+            centavos -= nota100 * 10000;
+            System.out.println(nota100 + " nota(s) de R$ 100.00");
+        }
+        if (centavos % 5000 >= 0) {
+            nota50 = centavos / 5000;
+            centavos -= nota50 * 5000;
+            System.out.println(nota50 + " nota(s) de R$ 50.00");
+        }
+        if (centavos % 2000 >= 0) {
+            nota20 = centavos / 2000;
+            centavos -= nota20 * 2000;
+            System.out.println(nota20 + " nota(s) de R$ 20.00");
+        }
+        if (centavos % 1000 >= 0) {
+            nota10 = centavos / 1000;
+            centavos -= nota10 * 1000;
+            System.out.println(nota10 + " nota(s) de R$ 10.00");
+        }
+        if (centavos % 500 >= 0) {
+            nota5 = centavos / 500;
+            centavos -= nota5 * 500;
+            System.out.println(nota5 + " nota(s) de R$ 5.00");
+        }
+        if (centavos % 200 >= 0) {
+            nota2 = centavos / 200;
+            centavos -= nota2 * 200;
+            System.out.println(nota2 + " nota(s) de R$ 2.00");
+        }
+        System.out.println("MOEDAS:");
+        
+        if (centavos % 100 >= 0) {
+            moeda1 = centavos / 100;
+            centavos -= moeda1 * 100;
+            System.out.println(moeda1 + " moeda(s) de R$ 1.00");
+        }
+        if (centavos % 50 >= 0) {
+            moeda50 = centavos / 50;
+            centavos -= moeda50 * 50;
+            System.out.println(moeda50 + " moeda(s) de R$ 0.50");
+        }
+        if (centavos % 25 >= 0) {
+            moeda25 = centavos / 25;
+            centavos -= moeda25 * 25;
+            System.out.println(moeda25 + " moeda(s) de R$ 0.25");
+        }
+        if (centavos % 10 >= 0) {
+            moeda10 = centavos / 10;
+            centavos -= moeda10 * 10;
+            System.out.println(moeda10 + " moeda(s) de R$ 0.10");
+        }
+        if (centavos % 5 >= 0) {
+            moeda5 = centavos / 5;
+            centavos -= moeda5 * 5;
+            System.out.println(moeda5 + " moeda(s) de R$ 0.05");
+        }
+        if (centavos % 1 >= 0) {
+            moeda01 = centavos;
+            System.out.println(moeda01 + " moeda(s) de R$ 0.01");
+        }
+    }
 }
+
---
+++ src/segundoDesafio/SegundoDesafioBonus.java
@@ -0,0 +1,70 @@
+package segundoDesafio;
+
+import java.util.Scanner;
+
+public class SegundoDesafioBonus {
+    public static void main(String[] args) {
+        executarDesafio();
+    }
+
+
+    //Método para chamar a resolução do desafio lendo input do valor
+    private static void executarDesafio() {
+        Scanner scanner = new Scanner(System.in);
+        float valor = scanner.nextFloat();
+        int centavos = Math.round(valor * 100);                                             //Multiplica o valor por 100 para tratar o valor como sendo unicamente em centavos
+        quantidadeNotas(centavos, saberNotaDaVez(centavos));
+    }
+
+
+    //Método criado para o funcionamento da recursividade, determina qual a moeda vai ser usada para próxima iteração
+    ////fazendo o calculo pra saber qual a menor moeda que o valor ainda é divisível
+    private static int saberNotaDaVez(int centavos) {
+        if (centavos / 10000 > 0) {
+            return 10000;
+        } else if (centavos / 5000 > 0) {
+            return 5000;
+        } else if (centavos / 2000 > 0) {
+            return 2000;
+        } else if (centavos / 1000 > 0) {
+            return 1000;
+        } else if (centavos / 500 > 0) {
+            return 500;
+        } else if (centavos / 200 > 0) {
+            return 200;
+        } else if (centavos / 100 > 0) {
+            return 100;
+        } else if (centavos / 50 > 0) {
+            return 50;
+        } else if (centavos / 25 > 0) {
+            return 25;
+        } else if (centavos / 10 > 0) {
+            return 10;
+        } else if (centavos / 5 > 0) {
+            return 5;
+        } else {
+            return 1;
+        }
+    }
+
+    private static void quantidadeNotas(int centavos, int notaDaVez) {                      //Método recursivo que usa o método saberNotaDaVez para determinar qual o funcionamento correto
+        int qntNota = (centavos / (notaDaVez));                                             //Calcula a quantidade de notas que são necessarias.
+        centavos -= qntNota * notaDaVez;                                                    //Atualiza o restante em centavos subtraindo o que já foi contado, preparando para a próxima iteração
+
+        if (notaDaVez == 100) {                                                             //Funcionamento caso a moeda da vez seja de 1 real
+            System.out.println(qntNota + "  moeda(s) de R$ " + notaDaVez / 100 + ".00");    //Exibe a quantidade de moeda(s) e o valor de cada divide o valor por 100 revertendo para real a conversão anterior para centavos
+            quantidadeNotas(centavos, saberNotaDaVez(centavos));                            //Chama a próxima iteração
+        } else if (notaDaVez < 100 && notaDaVez > 5) {                                      //Funcionamento caso a moeda da vez seja de 50 25 ou 10 centavos
+            System.out.println(qntNota + "  moeda(s) de R$ 00." + notaDaVez);               //Não é necessário converter para centavos já que são as menores casas
+            quantidadeNotas(centavos, saberNotaDaVez(centavos));
+        } else if (notaDaVez == 5) {                                                        //Funcionamento caso a moeda da vez seja de 5 centavos
+            System.out.println(qntNota + "  moeda(s) de R$ 00.0" + notaDaVez);
+            quantidadeNotas(centavos, saberNotaDaVez(centavos));
+        } else if (notaDaVez == 1) {                                                        //Funcionamento caso a moeda da vez seja a de 1 centavo, como é o menor valor possivel, não é necessário chamar o método novamente,
+            System.out.println(qntNota + "  moeda(s) de R$ 00.0" + notaDaVez);              // encerrando a recursividade, mas sempre imprime 0.01 já q é o break point, enquanto os outros valores que não sao utilzados não são impressos
+        } else {
+            System.out.println(qntNota + "  nota(s) de R$ " + notaDaVez / 100 + ".00");     //Funcionamento caso a moeda da vez seja nota
+            quantidadeNotas(centavos, saberNotaDaVez(centavos));
+        }
+    }
+}
